f87f8c8df8340893a3ab35565f220efd5c08e492
Saniya-Gupte <saniya.gupte18@gmail.com>
2022-10-28 15:26:58 +0530

    task1 complete

--- js/script.js
+++ js/script.js
@@ -15,3 +15,13 @@ nextBtn.addEventListener("click", ()=> {
 })
 
 backBtn.addEventListener("click", () => form.classList.remove('secActive'));
+
+function calculateAge(){
+    var dob = new Date(document.getElementById('bdate').value);
+    var month_diff = Date.now() - dob.getTime();
+    var age_dt = new Date(month_diff);
+    var year = age_dt.getUTCFullYear();
+    var age = Math.abs(year - 1970);
+    document.getElementById('age').value = age.toString();
+}
+
