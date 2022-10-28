eaa5adc5dbe40a071ba37f5ea275ab8e0feb5487
Sayali <sayalipanch17@gmail.com>
2022-10-28 15:48:24 +0530

    Task 1 modified

--- js/script.js
+++ js/script.js
@@ -16,12 +16,19 @@ nextBtn.addEventListener("click", ()=> {
 
 backBtn.addEventListener("click", () => form.classList.remove('secActive'));
 
-function calculateAge(){
-    var dob = new Date(document.getElementById('bdate').value);
-    var month_diff = Date.now() - dob.getTime();
-    var age_dt = new Date(month_diff);
-    var year = age_dt.getUTCFullYear();
-    var age = Math.abs(year - 1970);
-    document.getElementById('age').value = age.toString();
+function getAge() {
+    var today = new Date();
+    var birthDate = new Date(document.getElementById('bdate').value);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    if(age<0){
+        alert("Invalid Input!");
+    }
+    else{
+        document.getElementById('age').value = age.toString();
+    }
+    
 }
-
