b51417c2a6f83c81c9855ec1d33c815b26ef90a0
Sayali <sayalipanch17@gmail.com>
2022-10-30 16:47:43 +0530

    modified

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,9 +1,7 @@
 var li_elements = document.querySelectorAll(".wrapper_left ul li");
 var item_elements = document.querySelectorAll(".item");
-
 for (var i = 0; i < li_elements.length; i++) {
-  li_elements[i].addEventListener("click", function() 
-  {
+  li_elements[i].addEventListener("click", function() {
     li_elements.forEach(function(li) {
       li.classList.remove("active");
     });
